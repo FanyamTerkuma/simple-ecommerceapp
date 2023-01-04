e4e4dbceb13991ce5855e1e884c808469e5c31b5
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-01-04 19:01:55 +0100

    Removed packages,files and dependencies that were no longer needed

--- server/index.js
+++ server/index.js
@@ -2,17 +2,15 @@ const express = require('express');
 const mysql = require('mysql2');
 const multer = require('multer');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('file'));
+app.use('/products', express.static('./uploads'));
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, '/uploads');
+		cb(null, './uploads');
 	},
 	filename: function (req, file, cb) {
 		cb(null, Date.now() + file.originalname);
@@ -27,18 +25,6 @@ const db = mysql.createConnection({
 	database: 'ecommercedb'
 });
 
-const { parsed: config } = dotenv.config();
-const BASE_URL = `GET https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`; //temporary baseurl
-
-const auth = {
-	userName: config.CLOUD_NAME,
-	password: config.API_SECRET
-};
-
-app.get('/', (req, res) => {
-	res.json('backend reached');
-});
-
 app.get('/products', (req, res) => {
 	const query = 'SELECT * FROM products';
 	db.query(query, (err, data) => {
@@ -51,7 +37,7 @@ app.post('/products', upload.single('file'), (req, res) => {
 	const name = req.body.name;
 	const description = req.body.description;
 	const price = req.body.price;
-	const image = req.file.file;
+	const image = 'http://localhost:3001/products/' + req.file.filename;
 
 	const query = 'INSERT INTO products(name,description,price,image) VALUES(?,?,?,?)';
 	db.query(query, [name, description, price, image], (err, data) => {
